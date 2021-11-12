93b0f139015e19f39570ac39e9f819f5d3e50b3c
charlietruong <charlie.m.truong@gmail.com>
2021-11-12 00:03:24 -0600

    Add wink nlp

--- hacker-news-jobs/src/chrome/content.ts
+++ hacker-news-jobs/src/chrome/content.ts
@@ -2,6 +2,9 @@ import { ChromeMessage, MessageType } from "../types";
 import HackerNews from './hn-api';
 import HackerNewsItem from './hn-api';
 
+const winkNLP = require('wink-nlp');
+const model = require('wink-eng-lite-web-model');
+
 async function getKidIdsFromStory(storyId: number): Promise<number[] | null> {
     try {
         return await HackerNews.getItem(storyId)
@@ -46,6 +49,16 @@ const messagesFromReactAppListener = (
                     console.log(jobPostings)
                 })
         }
+        const nlp = winkNLP(model)
+        // Acquire "its" and "as" helpers from nlp.
+        const its = nlp.its;
+        const as = nlp.as;
+
+        const text = `Its quarterly profits jumped 76% to $1.13 billion for the three months to December, from $639million of previous year.`;
+        const doc = nlp.readDoc(text);
+
+        doc.entities().each((e: any) => e.markup());
+        console.log(doc.out(its.markedUpText));
     }
 }
 
